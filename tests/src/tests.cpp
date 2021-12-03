efa9c0a2f2fdd6efb1ed4c187d3e02a96cce2d7d
PeterSandager <91264172+PeterSandager@users.noreply.github.com>
2021-12-03 09:57:35 +0100

    Update tests.cpp

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1,7 +1,6 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "../include/catch.hpp"
-#include "../src/Duration.hpp"
-
+#include "Duration.h"
 // See Catch2's documentation: https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#scaling-up
 
 // Add your test cases. Dont forget to include header file.
